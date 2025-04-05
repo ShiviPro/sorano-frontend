f5004236d109245b1dc7c5d5b3b6d2abe4ce9b6f
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-04-05 11:26:25 +0530

    feat: add real event names

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,57 @@ const App = () => {
   const allEvents = [
     {
       id: 1,
-      name: "Event 1",
-      dateAndTime: "April 06, 2025 at 7PM IST",
+      title: "The Drawing Competition 2025",
+      dateAndTime: "Sunday, April 06, 2025 at 7:30 PM IST",
       type: "Online",
+      tags: [
+        "Art",
+        "Drawing",
+        "Figure Drawing",
+        "Illustration",
+        "Life Drawing",
+      ],
     },
     {
       id: 2,
-      name: "Event 2",
-      dateAndTime: "May 25, 2025 at 3PM IST",
-      type: "Offline",
+      title: "Fiber Arts Showcase: Unleash Your Creativity",
+      dateAndTime: "May 25, 2025 at 3:30PM IST",
+      type: "Online",
+      tags: ["Art", "Yarn", "Creativity", "Fiber Arts", "Felting"],
     },
     {
       id: 3,
-      name: "Event 3",
-      dateAndTime: "August 12, 2025 at 8PM IST",
-      type: "Online",
+      title: "Paint In Central Park (Bucket List Event)",
+      dateAndTime: "August 12, 2025 at 10:30AM IST",
+      type: "Offine",
+      tags: ["Art", "Painting", "Illustration"],
     },
     {
       id: 4,
-      name: "Event 4",
+      title: '"The Hunchback of Notre Dame" by Victor Hugo - Session 1 of 2',
       dateAndTime: "October 17, 2025 at 2PM IST",
-      type: "Offline",
+      type: "Offine",
+      tags: [
+        "Classic Books",
+        "Literature",
+        "Novel Reading",
+        "Authors",
+        "Writing",
+      ],
     },
     {
       id: 5,
-      name: "Event 5",
+      title:
+        "A New World Begins: French Revolution by Jeremy D. Popkin | Book Club",
       dateAndTime: "November 11, 2025 at 9AM IST",
       type: "Offline",
+      tags: [
+        "Non Fiction Book Club",
+        "Intellectual Discussions",
+        "History",
+        "French",
+        "Society  ",
+      ],
     },
   ];
   const [aptEvents, setAptEvents] = useState(allEvents);
@@ -68,14 +92,14 @@ const App = () => {
             <div className="row">
               <div className="col-md-4">
                 <img
-                  src={`https://placehold.co/800x600?text=${event.name}`}
-                  alt={event.name}
+                  src={`https://placehold.co/800x600?text=${event.title}`}
+                  alt={event.title}
                   className="card-img-start img-fluid"
                 />
               </div>
               <div className="col-md-8 ps-0">
                 <div className="card-body">
-                  <h2 className="card-title display-5">{event.name}</h2>
+                  <h2 className="card-title display-5">{event.title}</h2>
                   <p className="card-text fs-5 fw-light">
                     Date: {event.dateAndTime}
                   </p>
