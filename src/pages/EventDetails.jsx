72ce9c22ddd0c2625beaef5489b8af7c35fa9c53
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-04-08 21:18:38 +0530

    style: add apt spacing on details page

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -6,76 +6,94 @@ const EventDetails = () => {
   const event = allEvents.find((event) => event.id == eventId);
 
   return (
-    <main className="container py-4">
-      <article className="row">
-        <div className="col-md-8">
-          <h2>{event.title}</h2>
-          <p>Hosted By:</p>
-          {event.presentors.map((presentor) => (
-            <p key={presentor}>
-              <strong>{presentor}</strong>
-            </p>
-          ))}
-
-          <section>
-            <img src={event.posterUrl} alt={event.title} />
-          </section>
-
-          <section>
-            <h3>Details:</h3>
-            <pre style={{ fontFamily: "inherit" }} className="fs-5 fw-light">
-              {event.description}
-            </pre>
-          </section>
-
-          <section>
-            <h3>Additional Information:</h3>
-            {event.additionalInfo.length > 0 || <p>None</p>}
-            <ul>
-              {event.additionalInfo.length > 0 &&
-                event.additionalInfo.map((currInfo) => (
-                  <li key={currInfo}>{currInfo}</li>
-                ))}
-            </ul>
-          </section>
-
-          <section>
-            <h3>Event Tags:</h3>
-            {event.tags.map((currTag) => (
-              <button key={currTag} className="btn btn-primary me-3">
-                {currTag}
-              </button>
+    <main className="py-4 bg-light">
+      <div className="container">
+        <article className="row">
+          <div className="col-md-8">
+            <h2>{event.title}</h2>
+            <p className="mb-0">Hosted By:</p>
+            {event.presentors.map((presentor) => (
+              <p key={presentor}>
+                <strong>{presentor}</strong>
+              </p>
             ))}
-          </section>
-        </div>
-        <aside className="col-md-4">
-          <section className="card">
-            <div className="card-body">
-              <p>
-                <i className="fa-solid fa-clock"></i> {event.dateAndTime}
-              </p>
-              <p>
-                <i className="fa-solid fa-location-dot"></i> {event.venue}
-              </p>
-              <p className="mb-0">
-                <i className="fa-solid fa-dollar-sign"></i>{" "}
-                {event.price == 0 ? "Free" : event.price}
-              </p>
-            </div>
-          </section>
-          <section>
-            <h3>Speakers: ({event.presentors.length})</h3>
-            <div className="row">
-              {event.presentors.map((currPresentor) => (
-                <div key={currPresentor} className="col-6 rounded bg-light p-3">
-                  {currPresentor}
-                </div>
+
+            <section className="pt-3">
+              <img src={event.posterUrl} alt={event.title} />
+            </section>
+
+            <section className="pt-3">
+              <h3>Details:</h3>
+              <pre style={{ fontFamily: "inherit" }} className="fs-5 fw-light">
+                {event.description}
+              </pre>
+            </section>
+
+            <section>
+              <h3>Additional Information:</h3>
+              {event.additionalInfo.length > 0 || <p>None</p>}
+              <ul>
+                {event.additionalInfo.length > 0 &&
+                  event.additionalInfo.map((currInfo) => (
+                    <li key={currInfo}>{currInfo}</li>
+                  ))}
+              </ul>
+            </section>
+
+            <section>
+              <h3>Event Tags:</h3>
+              {event.tags.map((currTag) => (
+                <button key={currTag} className="btn btn-primary me-3">
+                  {currTag}
+                </button>
               ))}
+            </section>
+          </div>
+          <aside className="col-md-4 mt-2">
+            <section className="card">
+              <div className="card-body p-4 pb-2">
+                <div className="d-flex align-items-center pb-3">
+                  <p className="me-2">
+                    <i className="fa-solid fa-clock"></i>
+                  </p>{" "}
+                  <p>{event.dateAndTime}</p>
+                </div>
+                <div className="d-flex align-items-center pb-3">
+                  <p className="me-2">
+                    <i className="fa-solid fa-location-dot"></i>
+                  </p>{" "}
+                  <p>{event.venue}</p>
+                </div>
+                <div className="d-flex align-items-center">
+                  <p className="me-2">
+                    <i className="fa-solid fa-dollar-sign"></i>
+                  </p>{" "}
+                  <p>{event.price == 0 ? "Free" : event.price}</p>
+                </div>
+              </div>
+            </section>
+            {event.presentors.length > 0 && (
+              <section className="pt-4 mt-2">
+                <h3 className="">Speakers: ({event.presentors.length})</h3>
+                <div className="row ms-0 mt-3 mb-0">
+                  {event.presentors.map((currPresentor) => (
+                    <div key={currPresentor} className="col-6 card">
+                      <div className="card-body p-2">
+                        <p className="card-text fw-bold">{currPresentor}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            )}
+            <div className="row mt-4 pt-2">
+              <div className="text-center">
+                <button className="col-md-6 btn btn-primary">RSVP</button>
+              </div>
             </div>
-          </section>
-          <button className="btn btn-primary">RSVP</button>
-        </aside>
-      </article>
+          </aside>
+        </article>
+      </div>
     </main>
   );
 };
